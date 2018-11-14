93dd4cfa416dc1fab30ddaf80c5aef6f436e8a1d
MOBOLA MORONTO <t2wycemo@gmail.com>
2018-11-14 04:56:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -46,10 +46,10 @@ end
 def draw?(board)
       if !won?(board) && full?(board)
         return true
-      elsif !won?(board) && !full?(board)
+      elsif !won?(board) && !full?(board) || won?(board)
         return false
-      elsif won?(board)
-        return false
+      #elsif won?(board)
+        #return false
       end 
     end     
 end   
