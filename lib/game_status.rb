11fd629813ab0a12932b723c83493359b9ac34b7
MOBOLA MORONTO <t2wycemo@gmail.com>
2018-11-14 03:32:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,35 @@
 # Helper Method
+
+ board = [ " ", " ", " ", " ", " ", " ", " ", " ", " " ]
+ 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 4, 8], 
+  [6, 4, 2], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8]
+  ]
+  
+
+def won?(board)
+ WIN_COMBINATIONS.each do |win_combo|
+    win_combo.each{|win_index|}
+  win_index_1 = [ ]
+  if board[win_index[0]] == "X" && board[win_index[1]] == "X" && board[win_index[2]] == "X" ||
+  #return win_combo
+  #elsif
+    board[win_index[0]] == "O" && board[win_index[1]] == "O" && board[win_index[2]] == "O"
+  return win_combo
+  end
+ end
+  return false
+end
